eed77df1f00cbbabb30fa3b75dab436d632680b8
taghassan <taghassan54@gmail.com>
2025-08-17 13:00:30 +0400

    add blocked Accounts functions

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -23,6 +23,7 @@ export class PluginRequestEncryptionClient extends Plugin {
       return config;
     })
 
+
   }
 }
 
--- src/server/plugin.ts
+++ src/server/plugin.ts
@@ -4,6 +4,56 @@ import {decodeEncodedQueryMiddleware }from './middleware/decodeEncodedQuery'
 import { Context, Next } from '@nocobase/actions';
 import { DecodeResult, useDecodeEncodedQuery } from './hooks/useDecodeEncodedQuery';
 
+
+export function userPreCheckMiddleware() {
+  return async (ctx: Context, next: Next) => {
+    // نستهدف فقط تسجيل الدخول
+    if (ctx.path === '/api/auth:signIn' && ctx.method === 'POST') {
+      const { account, password } = ctx.action.params.values || {};
+
+      console.log("username, email ",account, password);
+      const blockedAccounts = [];
+
+
+      // 👇 تحقق مخصص (مثلاً تمنع مستخدم باسم معيّن)
+      if (blockedAccounts.includes(account)) {
+        ctx.status = 423;
+        ctx.body = {
+          error: 'AccessDenied',
+          message: 'هذا المستخدم غير مسموح له بتسجيل الدخول',
+          errors:[
+            {
+              "message": ctx.i18n.t("The user is locked, please try again later.") ,
+              "code": "USER_LOCKED"
+            }
+          ]
+        };
+
+
+        return; // وقف هنا
+      }
+
+      // مثال: تحقق من نطاق الإيميل
+      if (account && !account.endsWith('@nocobase.com')) {
+        ctx.status = 423;
+        ctx.body = {
+          error: 'UnauthorizedDomain',
+          message: 'يُسمح فقط بمستخدمي nocobase.com',
+        };
+        return;
+      }
+
+
+
+      await next();
+    }
+
+
+    // مرر للخطوة التالية (تسجيل الدخول العادي)
+    await next();
+  };
+}
+
 export class PluginRequestEncryptionServer extends Plugin {
   async afterAdd() {}
 
@@ -39,6 +89,10 @@ export class PluginRequestEncryptionServer extends Plugin {
 
      */
 
+    // إدراج الميدل وير قبل جميع الـ routes
+    this.app.use(userPreCheckMiddleware(),{
+      before: ['auth']
+    });
   }
 
   async install() {}
